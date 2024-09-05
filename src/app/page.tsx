9220f688dd4be68aa3de44e9eaee25f034cdff53
Rodolfo Cuevas <50083009+rcuevaspantoja@users.noreply.github.com>
2024-09-04 22:23:44 -0400

    v1.03

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Background from "@/components/Background/Background";
 import MePerdonas from "@/components/MePerdonas/MePerdonas";
 
+
 export default function Home() {
+
   return (
     <main className="h-screen w-screen flex items-center justify-center">
       <Background />
